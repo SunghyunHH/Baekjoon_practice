cfac8ccdffe481fa596bbbf566bfd358d01101f1
HANSUNGHYUN <94526604+SunghyunHH@users.noreply.github.com>
2023-02-23 02:18:53 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -3,14 +3,18 @@
 using namespace std;
 #define M 1234567891
 #define r 31
-int main(){
-  int l;
-  int sum =0;
-  string n;
-  cin >> l >> n;
-  for (int i=0; i<n.length(); i++){
-    sum += ((n[i]-96) * pow(r,i));
-  }
-  cout << sum % M;
-  return 0;
+// 모듈러의 속성 -> mod에 대해 분배 법칙을 적용해야함
+// r에 대한 mod연산 * 문자열(a)에 대한 mod 연산 == (r * a) mod n = {(r mod a) * (r mod a)} mod n
+int main() {
+    int l;
+    int sum = 0;
+    long long R = 1;
+    string n;
+    cin >> l >> n;
+    for (int i = 0; i < n.length(); i++) {
+        sum += ((n[i] - 96) * R) % M;
+        R = (r * R) % M;
+    }
+    cout << sum;
+    return 0;
 }
